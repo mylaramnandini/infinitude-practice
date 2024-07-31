2ad439164721912cac7aa4062e3a806bc9874fed
mylaramnandini <mylaramnandini@gmail.com>
2024-07-31 10:18:09 +0530

    this is my commit

---
+++ list.py
@@ -0,0 +1,28 @@
+
+
+
+my_list = [10, 20, 30, 40,'nandini', 'krishna', 20, 10]
+print(my_list[2:5])
+print(my_list[4])
+my_list.append('python')
+print(my_list)
+my_list.insert(5,'reddy')
+print(my_list)
+list2 = my_list.copy()
+print(list2)
+#print(list2.clear())
+print(my_list.count(10))
+print(my_list.index(30))
+print(my_list.pop(4))
+print(my_list)
+my_list.remove(30)
+print(my_list)
+list2 =[1, 2, 7, 3, 9, 5 ]
+list2.sort()
+print(list2)
+list2.reverse()
+print(list2)
+my_list.extend(list2)
+print(my_list)
+
+
